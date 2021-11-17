413275a55482c8f8af4fb7db256c25a9f2875813
Antonio Raimundo <antonio.raimundo@vizir.com.br>
2021-11-17 14:24:56 -0300

    refactor: add missing default props

---
+++ src/stories/ProgressIndicator.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ProgressIndicator } from '../progress-indicator/progress-indicator';
+
+export default {
+  title: "Example/ProgressIndicator",
+  component: ProgressIndicator,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof ProgressIndicator>;
+
+const Template: ComponentStory<typeof ProgressIndicator> = (args) => (
+  <ProgressIndicator {...args} />
+);
+
+export const Default = Template.bind({});
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 24,
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 64,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 64,
+};
