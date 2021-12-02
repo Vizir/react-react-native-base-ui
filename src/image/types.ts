0f835dc3e677b256da000c42b0b8ab38fa0a5c1a
Guilherme Afonso <guilherme.afonso@vizir.com.br>
2021-12-02 15:00:43 -0300

    feat(image): update image props

--- src/image/types.ts
+++ src/image/types.ts
@@ -1,6 +1,18 @@
-import React from 'react';
+import { SizePropValue } from "@naturacosmeticos/natds-web/dist/hooks/useSizeStyleProp";
+import React from "react";
 
 export interface IImageProps {
-  alt: React.ImgHTMLAttributes<HTMLImageElement>['alt'];
-  src?: React.ImgHTMLAttributes<HTMLImageElement>['src'];
+  alt: React.ImgHTMLAttributes<HTMLImageElement>["alt"];
+  src: React.ImgHTMLAttributes<HTMLImageElement>["src"];
+  fallback?: string;
+  className?: string;
+  disableSelection?: boolean;
+  height?: SizePropValue;
+  maxHeight?: SizePropValue;
+  maxWidth?: SizePropValue;
+  radius?: boolean;
+  state?: boolean;
+  style?: React.ImgHTMLAttributes<HTMLImageElement>["style"];
+  width?: SizePropValue;
+  draggable?: boolean | "auto";
 }
