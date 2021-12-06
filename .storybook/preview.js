d7161fde60a8510be8fc16b59dc5407458fb570b
Guilherme Afonso <guilherme.afonso@vizir.com.br>
2021-12-06 14:37:40 -0300

    feat(theme): add theme selector

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
-import '@naturacosmeticos/natds-icons/dist/natds-icons.css';
+import "@naturacosmeticos/natds-icons/dist/natds-icons.css";
+import { ThemeProviderDecorator } from "./decorators";
+
+export { globalTypes } from "./globalTypes";
+
+export const decorators = [ThemeProviderDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,5 +12,6 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+    expanded: true,
   },
-}
+};
