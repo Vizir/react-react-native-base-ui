7ed635a953f350a03a7317dd29e21ed61789a979
Guilherme Afonso <guilherme.afonso@vizir.com.br>
2021-12-08 14:53:41 -0300

    feat(spacing): Create spacing component

---
+++ src/spacing/spacing.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Spacing as NatSpacing } from "@naturacosmeticos/natds-web";
+
+import { IWebSpacingProps } from "./types";
+
+export const Spacing = (props: IWebSpacingProps): React.ReactElement => {
+  const { children, className, display, style, testID, ...rest } = props;
+  return (
+    <NatSpacing
+      className={className}
+      display={display}
+      style={style}
+      data-testId={testID}
+      {...rest}
+    >
+      {children}
+    </NatSpacing>
+  );
+};
