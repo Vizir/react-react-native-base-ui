1dbbe60fc4730cbf50f6e597a97611e4fca6f849
jardellins <jardel@vizir.com.br>
2022-01-04 10:16:05 -0300

    update natds version 1.5.2

--- src/icon/icon.native.tsx
+++ src/icon/icon.native.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Icon as NatIcon} from '@naturacosmeticos/natds-rn';
 
-import { IIconProps } from './types';
+import { IRNIconProps } from './types';
 
-export const icon = ({name}: IIconProps): React.ReactElement => {
+export const icon = ({name}: IRNIconProps): React.ReactElement => {
   return (
     <NatIcon
      name={name}
--- src/icon/types.ts
+++ src/icon/types.ts
@@ -1,6 +1,9 @@
 import { IIconProps as WebIconProps } from "@naturacosmeticos/natds-web/dist/Components/Icon/Icon.props";
+import { IconProps as RNIconProps } from "@naturacosmeticos/natds-rn/build/lib/components/Icon/Icon.types";
 export interface IIconProps {
   testID?: string;
 }
+
+export interface IRNIconProps extends RNIconProps, IIconProps {}
 
 export interface IWebIconProps extends WebIconProps, IIconProps {}
